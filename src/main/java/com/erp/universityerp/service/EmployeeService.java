54067ba34495e80e2205d6b1958fcc302e1506e9
Suresh Gadri <dhangarsuresh05989@gmail.com>
2026-06-09 10:21:36 +0530

    Auto create student and employee users

--- src/main/java/com/erp/universityerp/service/EmployeeService.java
+++ src/main/java/com/erp/universityerp/service/EmployeeService.java
@@ -1,8 +1,9 @@
 package com.erp.universityerp.service;
 
 import com.erp.universityerp.entity.Employee;
+import com.erp.universityerp.entity.User;
 import com.erp.universityerp.repository.EmployeeRepository;
-
+import com.erp.universityerp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +15,28 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public Employee saveEmployee(Employee employee) {
-        return employeeRepository.save(employee);
+
+        Employee savedEmployee =
+                employeeRepository.save(employee);
+
+        User user =
+                userRepository.findByUsername(employee.getEmployeeCode());
+
+        if(user == null) {
+
+            user = new User();
+            user.setUsername(employee.getEmployeeCode());
+            user.setPassword("Gadri@#09");
+            user.setRole("EMPLOYEE");
+
+            userRepository.save(user);
+        }
+
+        return savedEmployee;
     }
 
     public List<Employee> getAllEmployees() {
@@ -35,7 +56,7 @@ public class EmployeeService {
         Employee existingEmployee =
                 employeeRepository.findById(id).orElse(null);
 
-        if (existingEmployee != null) {
+        if(existingEmployee != null) {
 
             existingEmployee.setEmployeeCode(employee.getEmployeeCode());
             existingEmployee.setName(employee.getName());
--- src/main/java/com/erp/universityerp/service/StudentService.java
+++ src/main/java/com/erp/universityerp/service/StudentService.java
@@ -1,7 +1,9 @@
 package com.erp.universityerp.service;
 
 import com.erp.universityerp.entity.Student;
+import com.erp.universityerp.entity.User;
 import com.erp.universityerp.repository.StudentRepository;
+import com.erp.universityerp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,24 +15,46 @@ public class StudentService {
     @Autowired
     private StudentRepository studentRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public Student saveStudent(Student student) {
-        return studentRepository.save(student);
+
+        Student savedStudent = studentRepository.save(student);
+
+        User user = userRepository.findByUsername(student.getEnrollmentNo());
+
+        if(user == null) {
+            user = new User();
+            user.setUsername(student.getEnrollmentNo());
+            user.setPassword("Gadri@#09");
+            user.setRole("STUDENT");
+
+            userRepository.save(user);
+        }
+
+        return savedStudent;
     }
 
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
-
     }
+
     public Student getStudentById(Long id) {
         return studentRepository.findById(id).orElse(null);
     }
+
     public void deleteStudent(Long id) {
         studentRepository.deleteById(id);
     }
+
     public Student updateStudent(Long id, Student student) {
-        Student existingStudent = studentRepository.findById(id).orElse(null);
+
+        Student existingStudent =
+                studentRepository.findById(id).orElse(null);
 
         if(existingStudent != null) {
+
             existingStudent.setName(student.getName());
             existingStudent.setEmail(student.getEmail());
             existingStudent.setPhone(student.getPhone());
